cba2e33449c05ca0692222676dc9e9b976491a36
Roberto Emilio <robertoemilio1@hotmail.com>
2026-05-18 13:51:49 -0300

    Revert "Erro linha 247 app relacionado ao Segmentos - 3"
    This reverts commit 65d1f4cf1164dd9f25e0110713b01c11a64012d6.

--- strava_client.py
+++ strava_client.py
@@ -122,18 +122,13 @@ def get_segment_efforts(segment_id: int) -> list[dict]:
             params={"per_page": 50, "page": page},
             timeout=10,
         )
-        #res.raise_for_status()
-        if res.status_code != 200:
-            st.error(f"Erro HTTP: {res.status_code}")
-            st.write(res.text)
-            st.stop()        
-    
-    return res.json()
-    #apagar
-        #batch = res.json()
-        #if not batch:
-        #    break
-        #all_efforts.extend(batch)
-        #page += 1
+        print("STATUS CODE:", res.status_code)
+        print("RESPONSE:", res.text)
+        res.raise_for_status()
+        batch = res.json()
+        if not batch:
+            break
+        all_efforts.extend(batch)
+        page += 1
 
-    #return sorted(all_efforts, key=lambda e: e["start_date_local"])
+    return sorted(all_efforts, key=lambda e: e["start_date_local"])
