5841ede6d3c77c99b2713201a0f346411bebcfb5
Roberto Emilio <robertoemilio1@hotmail.com>
2026-05-18 13:27:05 -0300

    Erro linha 247 app relacionado ao Segmentos - 2

--- strava_client.py
+++ strava_client.py
@@ -122,6 +122,8 @@ def get_segment_efforts(segment_id: int) -> list[dict]:
             params={"per_page": 50, "page": page},
             timeout=10,
         )
+        print("STATUS CODE:", res.status_code)
+        print("RESPONSE:", res.text)
         res.raise_for_status()
         batch = res.json()
         if not batch:
